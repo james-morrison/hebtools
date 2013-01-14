275d5c9591ddf3aa63fc624a18d57d506e4631e4
LE12JM <LE12JM@LEF027-02L.uhi.ad.local>
2013-01-14 11:55:04 +0000

    modified so time based sets and number of wave sets can be both be calculated

--- combined_module/wave_concat.py
+++ combined_module/wave_concat.py
@@ -17,49 +17,56 @@ from datetime import datetime
 
 buoys = ['Roag_Wavegen','Bragar_HebMarine2','Siadar_HebMarine1']
 buoys_root_path = 'D:\\Datawell\\'
-set_size = 'half_hour'
+time_based_stats = False
 
 def timestamp_to_nearest_half_hour(timestamp, set_length_seconds):
     unix_timestamp = time.mktime(timestamp.timetuple())
     return round(unix_timestamp/set_length_seconds)*set_length_seconds
 
-def arrays_to_df_excel(stats_arrays_tuple, buoy_name):
-    start_times, end_times, h_max, h_1_3_mean = stats_arrays_tuple
-    index_df = pd.DatetimeIndex(start_times)
+def arrays_to_df_excel(stats_dict, buoy_name):
+    index_df = pd.DatetimeIndex(stats_dict['start_times'])
     print index_df
-    end_df = pd.DataFrame(end_times, index=index_df, columns=['end_times'])
-    h_1_3_mean_df = pd.DataFrame(h_1_3_mean, index=index_df, 
+    end_df = pd.DataFrame(stats_dict['end_times'], index=index_df, columns=['end_times'])
+    h_1_3_mean_df = pd.DataFrame(stats_dict['h_1_3_mean'], index=index_df, 
                                  columns=['h_1_3_mean'])
-    h_max_df = pd.DataFrame(h_max, index=index_df, columns=['h_max'])
+    h_max_df = pd.DataFrame(stats_dict['h_max'], index=index_df, columns=['h_max'])
     set_df = h_max_df.join([h_1_3_mean_df,end_df])
-    set_df.save(buoys_root_path + 'wave_h_' + str(set_size) + 
+    set_df.save(buoys_root_path + 'wave_h_' + str(stats_dict['set_size']) + 
                 'set_' + buoy_name)
-    set_df.to_excel(buoys_root_path + 'wave_h_' + str(set_size) + 'set_' + 
+    set_df.to_excel(buoys_root_path + 'wave_h_' + str(stats_dict['set_size']) + 'set_' + 
                     buoy_name + '.xlsx')
-
+ 
 def get_stats_from_df(large_dataframe):
     large_dataframe = large_dataframe.sort()
-    #index = np.arange(set_size,len(large_dataframe),set_size)
-    start_times = []
-    end_times = []
-    h_max = []
-    h_1_3_mean = []
-    time_set = 1800
-    timestamp = large_dataframe.ix[0].name
-    last_timestamp = large_dataframe.ix[-1].name
-    first_nearest_halfhour = timestamp_to_nearest_half_hour(timestamp, time_set)
-    last_nearest_halfhour = timestamp_to_nearest_half_hour(last_timestamp, time_set)
-    index = np.arange(first_nearest_halfhour, last_nearest_halfhour, time_set)
+    
+    stats_dict = {'start_times':[], 'end_times':[], 'h_max':[], 
+                        'h_1_3_mean':[]}
+    if time_based_stats:                        
+        time_set = 1800
+        timestamp = large_dataframe.ix[0].name
+        last_timestamp = large_dataframe.ix[-1].name
+        first_nearest_halfhour = timestamp_to_nearest_half_hour(timestamp, time_set)
+        last_nearest_halfhour = timestamp_to_nearest_half_hour(last_timestamp, time_set)
+        index = np.arange(first_nearest_halfhour, last_nearest_halfhour, time_set)
+        set_size = 'half_hour'
+        stats_dict['set_size'] = set_size
+    else:
+        set_size = 100
+        stats_dict['set_size'] = set_size
+        index = np.arange(set_size,len(large_dataframe),set_size)
     print index
     for x in index:
-        #subset = large_dataframe.ix[x-set_size:x]
-        subset = large_dataframe.ix[datetime.utcfromtimestamp(x-time_set):datetime.utcfromtimestamp(x)]
+        if time_based_stats:                   
+            subset = large_dataframe.ix[datetime.utcfromtimestamp(x-time_set):datetime.utcfromtimestamp(x)]
+        else:
+            subset = large_dataframe.ix[x-set_size:x]
         if len(subset) != 0:
-            start_times.append(subset.index[0])
-            end_times.append(subset.index[-1])
-            h_1_3_mean.append(subset.wave_height_cm.order()[-(len(subset)/3):].mean())
-            h_max.append(subset.max()[0])
-    return (start_times, end_times, h_max, h_1_3_mean)            
+            stats_dict['start_times'].append(subset.index[0])
+            stats_dict['end_times'].append(subset.index[-1])
+            stats_dict['h_1_3_mean'].append(subset.wave_height_cm.order()[-(len(subset)/3):].mean())
+            stats_dict['h_max'].append(subset.max()[0])
+    "finished stats"
+    return stats_dict          
 
 def iterate_over_buoys(buoys):
     for buoy_name in buoys:
@@ -76,8 +83,8 @@ def iterate_over_buoys(buoys):
                 wave_height_df = pd.load('wave_height_dataframe')
                 large_dataframe = pd.concat([large_dataframe, wave_height_df])
         large_dataframe.save('large_wave_height_df')
-        stats_arrays_tuple = get_stats_from_df(large_dataframe)
-        arrays_to_df_excel(stats_arrays_tuple, buoy_name)
+        stats_dict = get_stats_from_df(large_dataframe)
+        arrays_to_df_excel(stats_dict, buoy_name)
         
 iterate_over_buoys(buoys)    
 
