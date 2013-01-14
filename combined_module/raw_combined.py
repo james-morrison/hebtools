90cd1629d2b7acfde21ff196db78c270130360cc
LE12JM <LE12JM@LEF027-02L.uhi.ad.local>
2013-01-14 13:59:03 +0000

    project description and comment detail

--- combined_module/raw_combined.py
+++ combined_module/raw_combined.py
@@ -265,36 +265,38 @@ class Error_Check():
         self.detect_4_by_std()
         
     def detect_error_waves(self, extrema_df):
-        """
-            Find values with status signal problem and is a peak or trough,
-            then adds a 'signal_error' boolean column to the DataFrame
+        """Find values with status signal problem and is a peak or trough,
+        then adds a 'signal_error' boolean column to the DataFrame
         """
         print "start detect_error_waves"
         error_wave_mask = extrema_df['sig_qual']>0
-        bad_extrem_indexes = np.where(error_wave_mask==True)
-        bad_waves = []
-        for index in bad_extrem_indexes[0]:
-            extrema_type = extrema_df['extrema'].ix[index]
-            bad_waves.append(index)
-            if extrema_type == -1:
-                bad_waves.append(index+1)
-            else:
-                bad_waves.append(index-1)	
-        boolean_error_waves = []
-        for element in range(len(extrema_df)):
-            boolean_error_waves.append(False)
-        boolean_error_waves_array = np.array(boolean_error_waves)
-        for element in bad_waves:
-            boolean_error_waves_array[element] = True
-        error_waves_df = pd.DataFrame(boolean_error_waves_array ,columns=['signal_error'], index = extrema_df.index )
+        error_waves_df = pd.DataFrame(error_wave_mask ,columns=['signal_error'], index = extrema_df.index )
+        # bad_extrem_indexes = np.where(error_wave_mask==True)
+        # bad_waves = []
+        # for index in bad_extrem_indexes[0]:
+            # extrema_type = extrema_df['extrema'].ix[index]
+            # bad_waves.append(index)
+            # if extrema_type == -1:
+                # bad_waves.append(index+1)
+            # else:
+                # bad_waves.append(index-1)	
+        # boolean_error_waves = []
+        # for element in range(len(extrema_df)):
+            # boolean_error_waves.append(False)
+        # boolean_error_waves_array = np.array(boolean_error_waves)
+        # for element in bad_waves:
+            # boolean_error_waves_array[element] = True
+        # error_waves_df = pd.DataFrame(boolean_error_waves_array ,columns=['signal_error'], index = extrema_df.index )
         extrems_plus_errors = extrema_df.join(error_waves_df)
         extrems_plus_errors = extrems_plus_errors.sort()
         extrems_plus_errors.save('self.extrems_plus_errors_with_extrema_and_errors')	
         self.extrema_with_errors = extrems_plus_errors
 
     def compare_std(self, disp_set):
-        """ 
-            compare values with four time standard deviation
+        """Compare values with the calculated four times standard deviation, 
+        returning a boolean array, if any values exceeds the comparison, 
+        that value for that record is stored as True, with the implication
+        that the record should be masked
         """
         mask = np.array([])
         columns = ['heave', 'north', 'west']
@@ -310,10 +312,11 @@ class Error_Check():
 
     
     def detect_4_by_std(self):
-        """ This function iterates through the displacements DataFrame in 2304 long
-        sets, generating statistics for each set including standard deviation 
-        the heave displacements are then compared against 4 times their standard 
-        deviation """
+        """This function iterates through the displacements DataFrame in 2304 
+        long sets, generating statistics for each set including standard 
+        deviation the heave displacements are then compared against 4 times 
+        their standard deviation 
+        """
         print "detect_4_by_std"
         raw_set_length = 2304
         four_times_std_heave_30_mins = []
