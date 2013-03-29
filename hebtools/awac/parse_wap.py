2e01438f2d31071a95a97f6c977116e68030e366
James Morrison <james.morrison@gmail.com>
2013-03-29 20:32:26 +0000

    parse_wap.py - removed wap_columns for df_columns, names which are easier to reference in pandas syntax. imports wave_power module and adds wavelength and wave_power
    wave_power.py - moved calculate_wavelength out wave_power.calculate

--- hebtools/awac/parse_wap.py
+++ hebtools/awac/parse_wap.py
@@ -12,26 +12,17 @@ and applied as an index then DataFrame is save and an Excel xlsx version
 import os 
 import pandas as pd
 from datetime import datetime
-
-wap_columns = ['Month', 'Day', 'Year', 'Hour', 'Minute', 'Second', 'Spectrum type', 
- 'Significant height (Hm0)', 'Mean 1/3 height (H3)', 'Mean 1/10 height (H10)',
- 'Maximum height (Hmax)', 'Mean Height (Hmean)', 'Mean  period (Tm02)', 
- 'Peak period (Tp)', 'Mean zerocrossing period (Tz)', 'Mean 1/3 Period (T3)',
- 'Mean 1/10 Period (T10)', 'Maximum Period (Tmax)', 'Peak direction (DirTp)',
- 'Directional spread (SprTp)', 'Mean direction (Mdir)', 'Unidirectivity index',
- 'Mean Pressure (dbar)', 'Mean AST distance (m)', 'Mean AST distance (Ice)(m)',
- 'No Detects', 'Bad Detects', 'Number of Zero-Crossings', 
- 'Current speed (wave cell)(m/s)','Current direction (wave cell)', 'Error Code']
+from hebtools.common import wave_power
  
 df_columns = ['month', 'day', 'year', 'hour', 'minute', 'second', 
               'spectrum_type', 'significant_height_Hm0',
               'mean_1_3_height_H3', 'mean_1_10_height_H10', 
-              'Maximum height_Hmax', 'mean_height_Hmean', 'mean_period_Tm02', 
+              'maximum_height_Hmax', 'mean_height_Hmean', 'mean_period_Tm02', 
               'peak_period_Tp', 'mean_zerocrossing_period_Tz', 
               'mean_1_3_period_T3', 'mean_1_10_period_T10', 
               'maximum_period_Tmax', 'peak_direction_DirTp', 
               'directional_spread_SprTp', 'mean_direction_mdir', 
-              'unidirectivity index', 'mean_pressure_dbar', 
+              'unidirectivity_index', 'mean_pressure_dbar', 
               'mean_ast_distance_m', 'mean_ast_distance_ice_m', 'no_detects', 
               'bad_detects', 'num_zero_cross', 'current_speed_wave_cell',
               'current_direction_wave_cell', 'error_code']
@@ -61,9 +52,19 @@ def load(path, name=""):
     date_times_index = pd.DatetimeIndex(date_times)
     wap_file.index = date_times_index
     wap_file = wap_file[wap_file.error_code==0]
+    def wavelength_func(record):
+        return wave_power.calculate_wavelength(record[df_columns[13]])    
+    def wave_power_func(record):
+        return wave_power.calculate(record[df_columns[7]], 
+                                    record[df_columns[13]], 
+                                    record[wavelength_col_name])    
+    wavelength_col_name = 'wavelength'
+    wap_file[wavelength_col_name] = wap_file.apply(wavelength_func, axis=1)
+    wap_file['wave_power'] = wap_file.apply(wave_power_func, axis=1)
     if name != '':
         wap_file.columns = [name + '_' + x for x in wap_file.columns]
     file_name = file_name[:-4]
     wap_file.save(file_name + '_wap_df')
     wap_file.to_excel(file_name + '_wap' + '.xlsx')
+    print wap_file
     
--- hebtools/common/wave_power.py
+++ hebtools/common/wave_power.py
@@ -4,7 +4,28 @@ depth = 65
 
 import math
 
-def calculate(sig_wave_height, peak_period):
+def calculate_wavelength(peak_period):
+    """
+        Takes the peak period and calculates
+        the wavelength for any depth in metres
+    """
+    energy_period = 0.9 * peak_period
+    numerator= gravity * ( energy_period**2 )
+    denominator= 2 * math.pi
+    first_result = numerator / denominator
+    estimated_wavelength = first_result
+
+    tan_h = math.tanh( ( ( 2 * math.pi ) * depth) / estimated_wavelength )
+    L = tan_h * first_result
+    diff= L - estimated_wavelength
+    while diff > 0.01 or diff < -0.01:
+        tan_h = math.tanh( ( (2 * math.pi) * depth)/ estimated_wavelength )
+        L = tan_h * first_result
+        diff = L - estimated_wavelength
+        estimated_wavelength = estimated_wavelength + ( 0.5 * diff )
+    return estimated_wavelength
+    
+def calculate(sig_wave_height, peak_period, wavelength = None):
     """ Given significant wave height in metres and peak period Tp in seconds
         a tuple with wave power in kW per metre wave crest and wavelength in
         metres is returned.
@@ -12,27 +33,6 @@ def calculate(sig_wave_height, peak_period):
 
     def calculate_wave_power(sig_wave_height, group_velocity):
         return gravity * density_seawater * ( (sig_wave_height ** 2) /16 ) * group_velocity
-
-    def calculate_wavelength(peak_period):
-        """
-            Takes the peak period and calculates
-            the wavelength for any depth in metres
-        """
-        energy_period = 0.9 * peak_period
-        numerator= gravity * ( energy_period**2 )
-        denominator= 2 * math.pi
-        first_result = numerator / denominator
-        estimated_wavelength = first_result
-
-        tan_h = math.tanh( ( ( 2 * math.pi ) * depth) / estimated_wavelength )
-        L = tan_h * first_result
-        diff= L - estimated_wavelength
-        while diff > 0.01 or diff < -0.01:
-            tan_h = math.tanh( ( (2 * math.pi) * depth)/ estimated_wavelength )
-            L = tan_h * first_result
-            diff = L - estimated_wavelength
-            estimated_wavelength = estimated_wavelength + ( 0.5 * diff )
-        return estimated_wavelength
 
     def calculate_wave_number(wavelength):
         return (2 * math.pi)/wavelength
@@ -51,10 +51,11 @@ def calculate(sig_wave_height, peak_period):
         result = 1 + (numerator / denominator)
         return 0.5 * result * celerity
         
-    def get_wave_power_in_kw_p_m(sig_wave_height, peak_period):
-        wavelength = calculate_wavelength(peak_period)
+    def get_wave_power_in_kw_p_m(sig_wave_height, peak_period, wavelength):
+        if wavelength == None:
+            wavelength = calculate_wavelength(peak_period)
         group_velocity = calculate_group_velocity(wavelength, peak_period)
         wave_power = float(calculate_wave_power(sig_wave_height, group_velocity))/1000
-        return wave_power, wavelength
+        return wave_power
         
-    return get_wave_power_in_kw_p_m(sig_wave_height_cm, peak_period)
+    return get_wave_power_in_kw_p_m(sig_wave_height, peak_period, wavelength)
